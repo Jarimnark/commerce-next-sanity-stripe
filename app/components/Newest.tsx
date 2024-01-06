23cd327999307af3541b7bbdef95b8ad46564457
Jarimnark <jarimnark@gmail.com>
2024-01-06 12:06:37 +0700

    initial3

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import category from "@/sanity/schemas/category";
 
 async function getData() {
-  const query = `*[_type == 'product'][0...4] | order(_createdAt desc) {
+  const query = `*[_type == "product"][0...4] | order(_createdAt desc) {
     _id,
       price,
       name,
